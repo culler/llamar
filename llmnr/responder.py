7c97683e11882ad904edb79f01a1ad5a0e22ad82
Marc Culler <none@none>
2014-10-10 07:07:10 -0500

    Added GPL v2+ licensing boilerplate.

--- llmnr/responder.py
+++ llmnr/responder.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2014 by Marc Culler and others.
+#
+# This file is part of Llamar.
+#
+# Llamar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Llamar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with QuerierD.  If not, see <http://www.gnu.org/licenses/>.
+#
+# References:
+# https://tools.ietf.org/html/rfc4795
+# http://msdn.microsoft.com/en-us/library/dd240328.aspx
+
 import socket, struct, select, time
 from random import random
 from .queries import query_ntoa, query_aton
--- llmnr/sender.py
+++ llmnr/sender.py
@@ -1,11 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2014 by Marc Culler and others.
+#
+# This file is part of Llamar.
+#
+# Llamar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Llamar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with QuerierD.  If not, see <http://www.gnu.org/licenses/>.
+#
+# References:
+# https://tools.ietf.org/html/rfc4795
+# http://msdn.microsoft.com/en-us/library/dd240328.aspx
+
 import socket, struct, time
 from random import random
 from .queries import query_ntoa
 from .packets import Packet, Question, ResourceRecord, LLMNR_addrs, LLMNR_PORT
 from .iproute import NetworkState
-
-# https://tools.ietf.org/html/rfc4795
-# http://download.microsoft.com/download/9/5/E/.../%5BMS-LLMNRP%5D.pdf 
 
 class Sender(object):
     """Object with an ask method which sends LLMNR queries of type A,
