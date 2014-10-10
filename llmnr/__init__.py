5e7c374270de614904198716e51b73d4e23b49fe
Marc Culler <none@none>
2014-10-10 08:56:20 -0500

    Renamed to LLamar.

--- llmnr/__init__.py
+++ llmnr/__init__.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 # Copyright© 2014 by Marc Culler and others.
 #
-# This file is part of Llamar.
+# This file is part of LLamar.
 #
-# Llamar is free software: you can redistribute it and/or modify
+# LLamar is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
-# Llamar is distributed in the hope that it will be useful,
+# LLamar is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with QuerierD.  If not, see <http://www.gnu.org/licenses/>.
+# along with LLamar.  If not, see <http://www.gnu.org/licenses/>.
 #
 # References:
 # https://tools.ietf.org/html/rfc4795
